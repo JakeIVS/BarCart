772a200c9e7b888aab5b66c822ceacf87c86761e
Jacob Ives <jacob.w.ives@gmail.com>
2023-10-02 15:04:29 -0600

    add more functionality to test server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(logger);
+
 router.get("/", (req, res) => {
+  console.log(req.query.name);
   res.send("User List");
 });
 
 router.get("/new", (req, res) => {
-  res.send("New User Form");
+  res.render("users/new", { firstName: "Test" });
 });
 
 router.post("/", (req, res) => {
-  res.send("Create User");
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("error");
+    res.render("users/new", { firstName: req.body.firstName }); // pass form data back into the form when encountering an error
+  }
 });
 
 router
   .route("/:id")
   .get((req, res) => {
+    console.log(req.user);
     res.send(`Get User with ID ${req.params.id}`);
   })
   .put((req, res) => {
@@ -25,9 +36,15 @@ router
     res.send(`Delete User with ID ${req.params.id}`);
   });
 
+const users = [{ firstName: "Jacob" }, { firstName: "Henry" }];
 router.param("id", (req, res, next, id) => {
-  console.log(id);
+  req.user = users[id];
   next();
 });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(logger);
+// middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // allows us to parse data from the body of an html document, such as form data
+app.use(express.json()); // allows us to parse json data
 
-app.get("/", (req, res) => {
-  console.log("Here");
-  res.render("index", { text: "World" });
-});
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 

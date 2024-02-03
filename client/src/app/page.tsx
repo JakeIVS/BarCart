2e8dec44675527957ab0997722327544a8f0a6b9
Jacob Ives <jacob.w.ives@gmail.com>
2024-02-02 20:08:32 -0700

    create default landing page as placeholder frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-around p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/3 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/2 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <h1 className="text-8xl font-extrabold font-title">BarCart</h1>
+    <main className="flex bg-gradient-to-tr from-slate-900 via-zinc-700 to-zinc-800 min-h-screen flex-col items-center justify-around p-24">
+      <div className=" text-neutral-300 flex flex-col">
+        <h1 className="text-8xl font-light">BarCart</h1>
+        <h2 className="font-extralight w-full text-2xl text-center">
+          Home bar inventory assistance
+        </h2>
       </div>
     </main>
   );

d801170ad8cbd734d087a0cc2434a137f509a51d
Jacob Ives <jacob.w.ives@gmail.com>
2024-02-26 18:45:43 -0700

    create default page for invalid routes

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center text-center text-neutral-300">
+      <h2 className="font-light text-6xl">Page not found</h2>
+      <p className="font-extralight text-lg">
+        The page you are looking for does not exist
+      </p>
+    </div>
+  );
+}

1217b9704a43f2839e0eda8dbfedae658c1910f0
Jacob Ives <jacob.w.ives@gmail.com>
2024-02-27 13:29:01 -0700

    format not found page

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,8 +1,11 @@
 export default function NotFound() {
   return (
-    <div className="min-h-screen flex flex-col justify-center text-center text-neutral-300">
-      <h2 className="font-light text-8xl">404</h2>
-      <p className="font-extralight text-lg">Page Not Found</p>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="w-fit text-center text-neutral-300">
+        <h1 className="font-extralight text-8xl">404</h1>
+        <h3 className=" outline outline-1 mb-1 outline-neutral-300"></h3>
+        <h2 className="font-extralight text-lg">Page Not Found</h2>
+      </div>
     </div>
   );
 }

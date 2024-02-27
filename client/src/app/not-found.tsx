42ae282c17938fb0e31f3aebc41b1cb737999eae
Jacob Ives <jacob.w.ives@gmail.com>
2024-02-27 13:17:20 -0700

    reword error screen

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,10 +1,8 @@
 export default function NotFound() {
   return (
     <div className="min-h-screen flex flex-col justify-center text-center text-neutral-300">
-      <h2 className="font-light text-6xl">Page not found</h2>
-      <p className="font-extralight text-lg">
-        The page you are looking for does not exist
-      </p>
+      <h2 className="font-light text-8xl">404</h2>
+      <p className="font-extralight text-lg">Page Not Found</p>
     </div>
   );
 }

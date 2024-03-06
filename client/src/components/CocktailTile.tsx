bd4a9d5106ecbe315c8d9c75686d87fab0202e5f
Jacob Ives <jacob.w.ives@gmail.com>
2024-03-06 15:17:22 -0700

    fix naming convention to match db and page

---
+++ client/src/components/CocktailTile.tsx
@@ -0,0 +1,7 @@
+export default function CocktailTile() {
+  return (
+    <div>
+      <h1>Cocktail info</h1>
+    </div>
+  );
+}
--- client/src/components/RecipeTile.tsx
+++
@@ -1,7 +0,0 @@
-export default function RecipeTile() {
-  return (
-    <div>
-      <h1>Recipe info</h1>
-    </div>
-  );
-}

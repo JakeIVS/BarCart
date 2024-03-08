7b48328b80a0979167b75e3c557958e1e3f7f1dc
Jacob Ives <jacob.w.ives@gmail.com>
2024-03-07 17:03:33 -0700

    add placeholder for list creation logic

--- client/src/app/cocktails/page.tsx
+++ client/src/app/cocktails/page.tsx
@@ -1,6 +1,9 @@
 import CocktailTile from "@/components/CocktailTile";
 
 export default function Cocktails() {
+
+  const cocktailList = //Create a <CocktailTiile> for each item in db cocktail table
+
   return (
     <div>
       <h1>Cocktails</h1>
